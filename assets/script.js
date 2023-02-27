a37ab0c828311891704d7acfe22fdc4692f7c651
jsen07 <56829664+jsen07@users.noreply.github.com>
2023-02-27 10:35:25 +0000

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-let questions = [
+let questions = [ // Quiz Questions
     {
     question: "What are people who write computer code?",
     c1: "Professors",
@@ -53,25 +53,25 @@ let choiceThree = document.getElementById('choice-3');
 let choiceFour = document.getElementById('choice-4');
 let time = 75;
 
-const leaderboard = JSON.parse(localStorage.getItem("leaderboard")) || [];
+const leaderboard = JSON.parse(localStorage.getItem("leaderboard")) || []; 
 setQuestion();
 
 
-const timer = document.getElementById("timer");
+const timer = document.getElementById("timer"); //adding a timer 
 const gameTimer = setInterval(function() {
-    time --;
+    time --;  //timer that countsdown
     timer.innerText= time;
 
-    if (time <= 0) {
-        clearInterval(gameTimer);
+    if (time <= 0) { 
+        clearInterval(gameTimer); //stop time when time is less than or = 0 then run save score funciton
         saveScore();
     }
-}, 1000);
+}, 1000); 
 
 
-function setQuestion () {
+function setQuestion () { //sets the questions to be displayed to the player
    
-    let currentQuiz = questions[currentQuestion];
+    let currentQuiz = questions[currentQuestion]; 
 
     questionTitle.innerText = currentQuiz.question;
     choiceOne.innerText = currentQuiz.c1;
@@ -81,42 +81,44 @@ function setQuestion () {
     
 }
 
-const btns = document.querySelectorAll('button[id^=choice]');
+const btns = document.querySelectorAll('button[id^=choice]'); // adds event listener for each button and checks if the button has correct answer
 btns.forEach(btn => {
     btn.addEventListener('click', checkAnswer);
 });
-function checkAnswer(e) {
+function checkAnswer(e) { //checks if the button clicked is correct or wrong
 
     let  playerScore = document.getElementById("score");
     let checker = document.getElementById('right-wrong');
 
     if(e.target.innerText === questions[currentQuestion].correct) {
-        score += 10;
+        score += 10; // add 10 points if answer is correct
         checker.innerHTML= "Correct!";
         currentQuestion++;
         setQuestion();
     }
 
     else {
-        time += - 10;
-        score += -5;
+        time += - 10; //if answer is incorrect - 10s 
+        score += -5; //if answer is incorrect - 5 points
         checker.innerHTML= "Wrong!";
         currentQuestion++;
         setQuestion();
     }
-    if (currentQuestion === questions.length -1){
-        clearInterval(gameTimer);
+    if (currentQuestion === questions.length -1){ //when no more questions, stop time and run save score function
+        clearInterval(gameTimer); 
         saveScore();
     }
-    playerScore.innerText = score;
+    playerScore.innerText = score; // sets player score
 
 
 }
 
 
-function saveScore() {
+function saveScore() { //function to save the user score
     document.getElementById('hide').style.display="none";
 
+    
+    //creates html elements to be used to display and save score of user into highscores
     let hsContainer = document.createElement("div");
     hsContainer.classList.add("hs-container");
 
@@ -170,6 +172,7 @@ function saveScore() {
     document.body.appendChild(titleHead);
     document.body.appendChild(hsContainer);
 
+    //display home button, save button and play again buton
 document.getElementById("homeBtn").addEventListener("click", redirectHome);
 document.getElementById("saveBtn").addEventListener("click", save);
 document.getElementById("playBtn").addEventListener("click", playAgain);
@@ -177,15 +180,15 @@ document.getElementById("playBtn").addEventListener("click", playAgain);
 }
 
 
-function redirectHome() {
+function redirectHome() { //takes player back to index page
     window.location.assign("index.html");
 }
 
-function playAgain() {
+function playAgain() { //restarts the game`
     window.location.assign("quiz.html");
 }
 function save() {
-
+//saves player score into local storage
     let playerName = document.getElementById("playerName");
     
     const player = {
@@ -196,7 +199,7 @@ function save() {
     
     leaderboard.push(player);
 
-    leaderboard.sort(function(a, b){return b.score - a.score});
+    leaderboard.sort(function(a, b){return b.score - a.score}); //sorts player score to display highest - lowest and limits the player objects to the size of 4
     leaderboard.splice(4);
         
     
