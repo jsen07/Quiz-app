3b644e62dbc1df9b58945f6bb921cddbdeac891d
jsen07 <56829664+jsen07@users.noreply.github.com>
2023-02-27 10:37:20 +0000

    Update highscore.js

--- assets/highscore.js
+++ assets/highscore.js
@@ -6,22 +6,22 @@ let currentHS = 0;
 const highscoreList = document.getElementById("highscore-list");
 const board = document.getElementById("leaderboard");
 
-
+//if no highscores saved
 if (highscores.length === 0) {
-    let play = document.createElement('h2');
+    let play = document.createElement('h2'); 
     play.innerText = "Currently no scores have been saved.";
     board.appendChild(play);
     
 }
 
 for(var i = 0; i < highscores.length; i++) {
-
+//creates li element for each score saved and displays onto html
 let currentHighscore = highscores[i];
 let play = document.createElement('li');
 play.innerText = currentHighscore.player + " - " + currentHighscore.score;
 highscoreList.appendChild(play);
 }
-
+//add home button
 document.getElementById('home').addEventListener("click", redirectHome);
 
 function redirectHome() {
